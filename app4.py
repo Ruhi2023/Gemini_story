4e2438298f6449d1741b4e3d992e3bfc60172f2a
Ruhi2023 <132193880+Ruhi2023@users.noreply.github.com>
2024-08-28 17:33:07 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -93,7 +93,7 @@ with st.form("Inputs or login"):
 if get_story:
     d = {"ugak":ugak,"uhfak": uhfak,"prompt": prompt,"Age":Age,"rs":rs,"g":g,"l":l}
     st.write("Your prompt " ,prompt)
-    ur1 = "http://0.0.0.0:5003/invocations"
+    ur1 = "https://0.0.0.0:5003/invocations"
     #data = {"inputs":d}
     payl = json.dumps(d)
     st.header("Response section")
