67a0d42cd520448d2c55abe4743aa41bd629c910
Ruhi2023 <132193880+Ruhi2023@users.noreply.github.com>
2024-08-30 13:57:51 +0530

    Update app4.py

--- pages/app4.py
+++ pages/app4.py
@@ -183,7 +183,7 @@ if get_story:
     prog_bar.progress(100)
     st.write("Your cover image and story has been generated. You will be redirected automatically or use the button below.")
     
-    if "story" in st.sessionstate:
+    if "story" in st.session_state:
         if st.button(":material/local_library: Read story"):
             st.switch_page("pages/Stories_page.py" )
     time.sleep(2)
